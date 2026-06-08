5b062f379631343a57600ff5a2b0b83ed7a1c73a
PATAN SURAJ KHAN <138108119+Surajkhan12@users.noreply.github.com>
2026-06-08 21:11:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -376,8 +376,9 @@ def handle_search(data):
 if __name__ == "__main__":
 
     socketio.run(
-        app,
-        host="0.0.0.0",
-        port=5000,
-        debug=True
-    )
+    app,
+    host="0.0.0.0",
+    port=5000,
+    debug=True,
+    allow_unsafe_werkzeug=True
+)
